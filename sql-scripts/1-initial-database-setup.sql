26cbe040048a782bfe35b961f34714c5c2232711
sandves <stian@Stians-MacBook-Air.local>
2013-10-07 11:47:20 +0200

    Added create statements

--- sql-scripts/1-initial-database-setup.sql
+++ sql-scripts/1-initial-database-setup.sql
@@ -1,3 +1,15 @@
-;;
-;; Put in the create table statements here...
-;;
+--
+-- Put in the create table statements here...
+--
+
+delimiter $$
+
+CREATE DATABASE `PictureBrowser` /*!40100 DEFAULT CHARACTER SET latin1 */$$
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(64) DEFAULT NULL,
+  `blocked` tinyint(1) DEFAULT NULL,
+  `external_id` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
