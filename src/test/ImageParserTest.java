3dfbd34973b4f4cc3747f96682f1c0a579b82f00
sandves <stian@sandve.org>
2013-10-09 22:07:46 +0200

    Removed some println calls

--- src/test/ImageParserTest.java
+++ src/test/ImageParserTest.java
@@ -16,12 +16,9 @@ public class ImageParserTest {
 	@Test
 	public void givenImageCeckThatReturnedJsonIsValid() {
 		Date sqlDate = new Date(0);
-		String date = sqlDate.toString();
 		Image img = new Image("url", 1, "description", sqlDate);
 		String json = ImageParser.getJsonFromImage(img);
-		System.out.println(date);
-		System.out.println(json);
-		//have to be 1970-01-01 01:00:00 because the program will detect the time zone
+		//has to be 1970-01-01 01:00:00 because the program will detect the time zone
 		//that our jvm is set to (which is GMT+1)
 		assertEquals(json, "{\"url\":\"url\",\"id\":1,\"description\":" +
 				"\"description\",\"createdTime\":\"1970-01-01 01:00:00\"}");
@@ -40,8 +37,7 @@ public class ImageParserTest {
 		images.add(img2);
 		
 		String json = ImageParser.getJsonFromImage(images);
-		System.out.println(json);
-		//have to be 1970-01-01 01:00:00 because the program will detect the time zone
+		//has to be 1970-01-01 01:00:00 because the program will detect the time zone
 		//that our jvm is set to (which is GMT+1)
 		assertEquals(json, "[{\"url\":\"url1\",\"id\":1,\"description\":" +
 				"\"description1\",\"createdTime\":\"1970-01-01 01:00:00\"}" + 
