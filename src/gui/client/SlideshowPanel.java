4d5326a81cbf7cc8bb1b3491502e786e6737e0f5
sandves <stian@Stians-MacBook-Air.local>
2013-10-16 21:16:48 +0200

    Began implementing the SlideshowPanel

---
+++ src/gui/client/SlideshowPanel.java
@@ -0,0 +1,54 @@
+package gui.client;
+
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import javax.swing.JPanel;
+
+public class SlideshowPanel extends JPanel {
+
+	private static final long serialVersionUID = -3943809441036747592L;
+	
+	private BufferedImage bi;
+	private int marginX = 0;
+	private int marginY = 0;
+
+	public SlideshowPanel() {
+		//Sets the panels preferred size to be the same as the screens size
+		this.setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
+	}
+
+	public void setImage(BufferedImage bi) {
+		this.bi = bi;
+		this.calculateMargins();
+		this.revalidate();
+		this.repaint();
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(bi, marginX, marginY, null);
+	}
+	
+	//This method will make sure that the image is drawn on the center
+	//of the panel
+	private void calculateMargins() {
+		setMarginX();
+		setMarginY();
+	}
+	
+	private int setMarginX() {
+		int panelWidth = this.getWidth();
+		int marginX = (panelWidth-bi.getWidth())/2;
+		return marginX;
+	}
+	
+	private int setMarginY() {
+		int panelHeight = this.getHeight();
+		int marginY = (panelHeight-bi.getHeight())/2;
+		return marginY;
+	}
+}
+
+
